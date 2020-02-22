50c5303ac43d9d622538fc4392c101fc17bd57f6
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-22 10:27:26 -0800

    starting writing content model

---
+++ utils/contentful.js
@@ -0,0 +1,47 @@
+const authorFields = [
+  {
+    id: 'name',
+    name: 'Name',
+    type: 'Symbol',
+    required: true
+  },
+  {
+    id: 'title',
+    name: 'Job Title',
+    type: 'Symbol',
+    required: true
+  },
+  {
+    id: 'company',
+    name: 'Company',
+    type: 'Symbol',
+    required: true
+  },
+  {
+    id: 'shortBio',
+    name: 'Short Bio',
+    type: 'Text',
+    required: true
+  },
+  {
+    id: 'email',
+    name: 'Email',
+    type: 'Symbol',
+    required: true
+  },
+  {
+    id: 'twitter',
+    name: 'Title',
+    type: 'Symbol',
+    required: true
+  },
+];
+
+const blogFields = [
+  {
+    id: 'title',
+    name: 'Title',
+    type: 'Symbol',
+    required: true
+  }
+]
